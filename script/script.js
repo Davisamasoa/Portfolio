339cff2b38ad419da89ff454553b466f35384d22
Davi Samuel <davisamasoa@gmail.com>
2022-09-27 17:46:59 -0300

    task list add

--- script/script.js
+++ script/script.js
@@ -1,13 +1,19 @@
 let btnVerMais = document.querySelector(".verMais");
-let hiddenProjects = document.querySelector(".none");
+let hiddenProjects = document.querySelectorAll(".none");
 
 btnVerMais.addEventListener("click", () => {
-	if (hiddenProjects.style.display == "none") {
-		hiddenProjects.style.display = "flex";
+	if (hiddenProjects[0].style.display == "none") {
+		for (let i = 0; i < hiddenProjects.length; i++) {
+			hiddenProjects[i].style.display = "flex";
+		}
+
 		btnVerMais.innerHTML = '<i class="bi bi-caret-up-fill"></i>';
-		window.location.href = "#music";
+		window.location.href = "#pomo";
 	} else {
-		hiddenProjects.style.display = "none";
+		for (let i = 0; i < hiddenProjects.length; i++) {
+			hiddenProjects[i].style.display = "none";
+		}
+
 		btnVerMais.innerHTML = '<i class="bi bi-caret-down-fill"></i>';
 		window.location.href = "#project";
 	}
