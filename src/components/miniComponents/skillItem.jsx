28ab8c55de11e16064b338647d6b1b4a8bdbd59e
Davi Samuel <davisamasoa@gmail.com>
2022-12-06 15:58:26 -0300

    skill item background add

--- src/components/miniComponents/skillItem.jsx
+++ src/components/miniComponents/skillItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export function SkillItem(props) {
 	return (
-		<li className="skillItem">
+		<li className="skillItem hover:scale-125">
 			<a href={props.tecLink} target="_blank" rel="noreferrer">
-				<img className="hover:scale-125" src={props.tecImg} alt={props.imgAlt} />
+				<img src={props.tecImg} alt={props.imgAlt} />
 			</a>
 		</li>
 	);
