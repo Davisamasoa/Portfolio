42217cf587c53cd25d457410849a8befb9e70b33
Davi Samuel <davisamasoa@gmail.com>
2023-12-15 14:49:42 -0300

    white bg defined as default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html className="dark" lang="pt-br">
+		<html lang="pt-br">
 			<body
 				className={`${inter.className} bg-bgColor dark:bg-darkbgColor text-textColor dark:text-darktextColor`}
 			>
