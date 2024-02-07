3ca42a9b7e937b2ea0e63e00215bc6dfc0a5abb2
Davi Machado <davisamasoa@gmail.com>
2024-02-07 00:29:15 -0300

    og image and page description changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Davi Samuel | Dev Full Stack Jr",
-	description:
-		"Portfólio web de Davi Samuel, um estudante de programação que busca sua primeira chance no mercado como desenvolvedor full-stack",
+	title: "Davi Samuel | Dev Full Stack",
+	description: "Portfólio de Davi Samuel, programador web Full Stack.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
