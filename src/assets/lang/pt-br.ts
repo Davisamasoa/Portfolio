008acffc9c2e14b60da82d9392f881e810cca28f
Davi Machado <davisamasoa@gmail.com>
2025-04-19 06:56:01 +0000

    age information changed

--- src/assets/lang/pt-br.ts
+++ src/assets/lang/pt-br.ts
@@ -8,7 +8,7 @@ export const pt_br = {
 
 	about: {
 		title: "Sobre mim",
-		text: "Olá, tudo bem? Me chamo Davi Machado, tenho 20 anos e nasci em Olinda - PE, mas moro em Belo Horizonte - MG desde os meus 2 anos de idade. As pessoas sempre me diziam que eu tinha uma certa facilidade com coisas relacionadas à tecnologia, desde mexer em um celular ou qualquer outro dispostivo eletrônico. Bom, uma coisa levou a outra e a minha paixão pela tecnologia foi se expandindo, desde então nunca me enxerguei trabalhando em outra área. E quando conheci a programação ela simplesmente caiu como uma luva nos meus interesses para a vida. Espero que possamos trabalhar juntos em breve!",
+		text: "Olá, tudo bem? Me chamo Davi Machado, tenho 21 anos e nasci em Olinda - PE, mas moro em Belo Horizonte - MG desde os meus 2 anos de idade. As pessoas sempre me diziam que eu tinha uma certa facilidade com coisas relacionadas à tecnologia, desde mexer em um celular ou qualquer outro dispostivo eletrônico. Bom, uma coisa levou a outra e a minha paixão pela tecnologia foi se expandindo, desde então nunca me enxerguei trabalhando em outra área. E quando conheci a programação ela simplesmente caiu como uma luva nos meus interesses para a vida. Espero que possamos trabalhar juntos em breve!",
 	},
 
 	skills: {
